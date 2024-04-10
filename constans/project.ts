f75607329430fe74aa9f643ff39d411b10a83f26
sbraco <steve.braco@sogeti.com>
2024-04-10 16:00:25 +0200

    add project

--- constans/project.ts
+++ constans/project.ts
@@ -27,6 +27,15 @@ export const dataProject = [
     stack: ['Next.js 14', 'TypeScript', 'Tailwind CSS', 'MongoDB', 'React Hook Form']
   },
   {
+    id: 5,
+    name: 'Hotel App',
+    description: 'This e-commerce site offers a wide range of products sourced from an API. Users can quickly find items by name and refine searches by category, company, color, and price range. Advanced filters ensure a tailored shopping experience, empowering users to discover products within their budget. With seamless navigation and efficient search tools, users can easily explore the extensive catalog to find the perfect item.',
+    img: '/assets/hotel.png',
+    site: 'https://air-hotel-react.vercel.app/products',
+    code: 'https://github.com/stevebraco/airHotel-react/tree/main',
+    stack: ['React', 'Redux', 'Styled-Components']
+  },
+  {
     id: 4,
     name: 'Audiophile',
     description: 'I focused on essential features to boost the shopping experience: streamlined cart management, intuitive checkout with validations, automatic pricing calculations including shipping and VAT, and a confirmation modal post-checkout. The site features responsive design, interactive element hover states, and cart persistence across sessions, leveraging localStorage.',
@@ -34,5 +43,15 @@ export const dataProject = [
     site: 'https://audiophile.vercel.app/',
     code: 'https://github.com/stevebraco/React-Audiophile/tree/main/audiophile-ecomm-website/audiophile-ecomm-app',
     stack: ['React', 'Redux', 'Sass']
-  }
+  },
+  {
+    id: 6,
+    name: 'Dine',
+    description: ' I crafted the Dine restaurant website, ensuring users see tailored content for Family Gatherings, Special Events, and Social Events tabs. I implemented error handling for the booking form: prompting "This field is required" for empty Name or Email Address fields, "Please use a valid email address" for incorrectly formatted emails, and "This field is incomplete" for empty Pick a date or Pick a time fields. The interface seamlessly adjusts to optimal layouts for different device screen sizes, while all interactive elements feature hover and focus states, enhancing user experience.',
+    img: '/assets/dine.webp',
+    site: 'https://dinner-restaurant.vercel.app/',
+    code: 'https://github.com/stevebraco/dinner-restaurant',
+    stack: ['Html', 'CSS', 'JavaScript']
+  },
+  
 ]
